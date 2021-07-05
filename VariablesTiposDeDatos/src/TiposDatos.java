685608d9f1e4337f4ee29a5cdd5b352f6e04fe6b
proivited <miguel.alexis.gomez.a@gmail.com>
2021-07-05 14:15:58 -0500

    variables de tipos de datos

---
+++ VariablesTiposDeDatos/src/TiposDatos.java
@@ -0,0 +1,29 @@
+
+public class TiposDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * si pones /** y enter te da comentario doc chidito
+		 */
+		
+		System.out.println("Hola Mundo");
+		
+		int numero1 = 10;
+		int numero2 = 12;
+		int multiplicacion = numero1 * numero2;
+		String cadena1 = "texto1";
+		String cadena2 = "texto2"; //las dobles para todas
+		char letra = 'Y'; //las simples son para un caracter
+		
+		String oracion = cadena1 + cadena2;
+		
+		boolean mayor18 = true;
+		
+		System.out.println(multiplicacion);
+		System.out.println(oracion);
+
+	}
+
+}
